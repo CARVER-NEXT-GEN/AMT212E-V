ea447b108c220d322bfb0a4a500eecaf70a3bef5
Kireiji02 <martmanaswin@gmail.com>
2024-12-28 02:27:29 +0700

    whatever

--- AMT212E-V_Micro-ROS/firmware/AMT212E-V_Micro-ROS_UART/Core/Inc/main.h
+++ AMT212E-V_Micro-ROS/firmware/AMT212E-V_Micro-ROS_UART/Core/Inc/main.h
@@ -78,6 +78,8 @@ void Error_Handler(void);
 #define T_SWDIO_GPIO_Port GPIOA
 #define T_SWCLK_Pin GPIO_PIN_14
 #define T_SWCLK_GPIO_Port GPIOA
+#define Emergency_Pin GPIO_PIN_10
+#define Emergency_GPIO_Port GPIOC
 #define T_SWO_Pin GPIO_PIN_3
 #define T_SWO_GPIO_Port GPIOB
 
